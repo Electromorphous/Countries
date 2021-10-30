82aa6ee0df8171c62feec6546096e77ab47245df
Electromorphous <pstation.474@gmail.com>
2021-10-31 01:37:29 +0530

    add themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ThemeContext from "./utility/ThemeContext";
+import Bruh from "./Bruh";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <ThemeContext>
+      hello world
+      <Bruh />
+    </ThemeContext>
+  );
 }
 
 export default App;
---
+++ src/utility/ThemeContext.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const ThemeUpdateContext = React.createContext();
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext);
+}
+
+export default function ThemeContext({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+
+      primary: {
+        light: "#fff",
+        main: "#fff",
+      },
+      textColor: {
+        main: "#000",
+      },
+    },
+  });
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+
+      primary: {
+        light: "#2B3743",
+        main: "#212E37",
+      },
+      textColor: {
+        main: "#fff",
+      },
+    },
+  });
+
+  const toggleTheme = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeProvider>
+  );
+}
